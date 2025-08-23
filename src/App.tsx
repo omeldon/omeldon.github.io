fcd99295569c5077f7d1ef4e219957e9c6c206bc
Rommel Dones <104125810+omeldon@users.noreply.github.com>
2025-08-23 14:09:36 +0800

    Add files via upload
    Signed-off-by: Rommel Dones <104125810+omeldon@users.noreply.github.com>

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PortWeb from './components/port';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <PortWeb />
+    </div>
+  );
+}
+
+export default App;
